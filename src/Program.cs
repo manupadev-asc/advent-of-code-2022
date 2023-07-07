e9f2f0029e9fd4c7fc226de647923c8fdaa0069b
manupadev-asc <manupa.samarawickrama@ascentic.se>
2023-07-07 14:47:22 +0530

    feat: setup file reading

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 
-class App{
-    static void Main(){
-        Console.WriteLine("Hello World");
+class App
+{
+    static void Main()
+    {
+
+        string[] data = File.ReadAllLines("input.txt");
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.WriteLine(data[i]);
+        }
     }
 }
