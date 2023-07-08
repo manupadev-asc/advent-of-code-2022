339febfcd9cbb32f841555d975fec294e66a7e23
manupadev-asc <manupa.samarawickrama@ascentic.se>
2023-07-08 19:54:58 +0530

    feat: complete day 1 task

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.Collections;
 
 class App
 {
+    static readonly string fileName = "input.txt";
     static void Main()
     {
+        List<List<int>> elves = new List<List<int>>();
 
-        string[] data = File.ReadAllLines("input.txt");
-        for (int i = 0; i < data.Length; i++)
+        if (!File.Exists(fileName)) return;
+        using StreamReader reader = new(fileName);
+        string? line;
+
+        List<int> elf = new List<int>();
+        while ((line = reader.ReadLine()) != null)
         {
-            Console.WriteLine(data[i]);
+
+            if (!line.Equals(""))
+            {
+                elf.Add(int.Parse(line));
+                continue;
+            }
+            elves.Add(elf);
+            elf = new List<int>();
         }
+        List<int> sums = new List<int>();
+        foreach (List<int> _elf in elves)
+        {
+            sums.Add(_elf.Sum());
+        }
+        Console.WriteLine(sums.Max());
     }
+
+
 }
